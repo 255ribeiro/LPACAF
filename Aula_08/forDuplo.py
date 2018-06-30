1f64d0d5cee9b24b1b1c15de924a11f4ce5355f3
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2018-06-29 23:13:57 -0300

    exemplo for duplo

---
+++ Aula_08/forDuplo.py
@@ -0,0 +1,24 @@
+#! -*-coding-:- utf8
+
+# criando listas
+lista1 = range(0,5,1)
+lista2 = ['a', 'b', 'c', 'd']
+
+#exibindo lista1
+print lista1
+
+# exibindo lista2
+print lista2
+
+#mensagem
+print 'inicio do for'
+
+listaXY = [] # iniciando lista vazia
+for i in range(len(lista1)): # prineiro for
+    listaAux = [] # iniciando lista auxiliar
+    print 'loop ', i , ' do primeiro for /n'
+    for j in range(len(lista2)):
+        pTemp = 'P(' + str(lista1[i])+ ',' + lista2[j] + ')' #criando ponto
+        listaAux.append(pTemp) #Adicionando ponto na lista auxiliar
+    listaXY.append(listaAux) Adicionando 
+    print listaXY # mostrando listaXY
