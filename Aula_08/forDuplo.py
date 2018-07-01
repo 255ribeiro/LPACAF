058a1e0a2fba96c00ef0bb7c0192b96f1459aa7d
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2018-07-01 01:39:33 -0300

    Organização das mensagems do arquivo

--- Aula_08/forDuplo.py
+++ Aula_08/forDuplo.py
@@ -1,26 +1,28 @@
-#! -*-coding-:- utf8
+#! python
+#! -*-encoding-:- utf8
 
 # criando listas
 lista1 = range(0,3,1)
 lista2 = ['a', 'b', 'c', 'd']
 
 #exibindo lista1
-print lista1
+print 'lista 1: ',lista1
 
 # exibindo lista2
-print lista2
+print 'lista 2: ', lista2
 
-#mensagem
-print 'inicio do for'
-
+#pular linha
+print '\n'
+#mensagem de início do primeiro loop
+print 'inicio do primeiro for \n'
 listaXY = [] # iniciando lista vazia
 for i in range(len(lista1)): # prineiro for
     listaAux = [] # iniciando lista auxiliar
-    print 'loop ', i , ' do primeiro for \n'
+    print 'loop ', i , ' do primeiro for' # 
     for j in range(len(lista2)):
         pTemp = 'P(' + str(lista1[i]) + ',' + lista2[j] + ')' #criando ponto
         listaAux.append(pTemp) #Adicionando ponto na lista auxiliar
         listaXY.append(listaAux) #Adicionando lista auxiliar na listaXY
-    print listaXY # mostrando listaXY
+    print listaXY, "\n" # mostrando listaXY
     
     
