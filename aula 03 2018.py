99bba08d524c342f71ce42ce1369df8901bebb25
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2018-07-02 02:16:10 -0300

    edição de comentários

--- aula 03 2018.py
+++ aula 03 2018.py
@@ -4,25 +4,21 @@ from __future__ import division
 import math as m
 
 # definicao da funcao blondel
-
-
 def blondel(h):
     # formula de blodel
     p = .63 - (2 * h)
-    # retorna o piso
+    # retorna o valor do piso do degrau
     return p
 
-
-# entrada de dados
-# desnivel da escada
+# entrada de dados:
+## desnivel da escada
 des = input('entre com desnivel ')
-# numero maxino de espelhos
+## numero maxino de espelhos
 espM = input('entre com o espelho maximo ')
 
 # calcula numero de espelhos
 nEsp = m.ceil(des/espM)
-
-
+# ostra numero de espelhos
 print "Numero de espelhos = ", nEsp
 
 # espelho real 3 casas decimais
@@ -32,5 +28,5 @@ print 'espelho real ', espR
 
 # chamada da funcao blondel
 piso = blondel(espR)
-
+# mostra piso do degrau
 print "piso = ", piso
